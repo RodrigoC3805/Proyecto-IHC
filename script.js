4c2d9808d5f3aa9bf6d889785b8f691ad8328faa
RodrigoC3805 <rodrigocastillo3805@gmail.com>
2025-11-22 21:59:21 -0500

    Ruta de camión agregada

--- script.js
+++ script.js
@@ -32,6 +32,16 @@ const coordenadasCamion = [
   [-12.05304105985056, -77.08499919520301],
   [-12.0534097365194, -77.08735924157892],
 ];
+const coordenadasRuta = [
+  [-12.0534097365194, -77.08735924157892],
+  [-12.05349037618645, -77.08624900029055],
+  [-12.053647395651087, -77.08605895937144],
+  [-12.05377330312814, -77.08510945739384],
+  [-12.054190371208751, -77.084841236506],
+  [-12.054930075278097, -77.08527307213885],
+  [-12.054798922225103, -77.08663563424908],
+  [-12.056265347116058, -77.08684258122724],
+];
 const pinUsuario = L.icon({
   iconUrl: "img/pin-user.png",
   iconSize: [90, 90],
@@ -52,6 +62,8 @@ const pinCamion = L.icon({
   iconSize: [50, 50],
   iconAnchor: [25, 50],
 });
+let camionMarker;
+const nuevaCoordenadaCamion = [-12.053661825195139, -77.08604971368443];
 
 // Listeners: Tarea Mapa
 document.getElementById("btn-basura").addEventListener("click", () => {
@@ -62,6 +74,11 @@ document.getElementById("btn-reciclaje").addEventListener("click", () => {
 });
 document.getElementById("btn-camion").addEventListener("click", () => {
   chooseView("map3");
+});
+document.addEventListener("keydown", (e) => {
+  if (e.key.toLowerCase() === "p") {
+    moverCamionYNotificar();
+  }
 });
 botonesMapa.forEach((boton) => {
   boton.addEventListener("click", () => {
@@ -136,7 +153,10 @@ function chooseView(mapView) {
   switch (mapView) {
     case "map1":
       map.eachLayer((layer) => {
-        if (layer instanceof L.Marker) {
+        if (
+          (layer instanceof L.Marker ||
+          layer instanceof L.Polyline)
+        ) {
           map.removeLayer(layer);
         }
       });
@@ -147,7 +167,8 @@ function chooseView(mapView) {
       break;
     case "map2":
       map.eachLayer((layer) => {
-        if (layer instanceof L.Marker) {
+        if ((layer instanceof L.Marker ||
+          layer instanceof L.Polyline)) {
           map.removeLayer(layer);
         }
       });
@@ -158,18 +179,36 @@ function chooseView(mapView) {
       break;
     case "map3":
       map.eachLayer((layer) => {
-        if (layer instanceof L.Marker) {
+        if (
+          (layer instanceof L.Marker ||
+          layer instanceof L.Polyline) && layer !== camionMarker
+        ) {
           map.removeLayer(layer);
         }
       });
       L.marker(coordenadaUsuario, { icon: pinUsuario }).addTo(map);
-      coordenadasCamion.forEach((coord) => {
-        L.marker(coord, { icon: pinCamion }).addTo(map);
+      coordenadasCamion.forEach((coord, index) => {
+        if (index == 1) {
+          camionMarker = L.marker(coord, { icon: pinCamion }).addTo(map);
+        } else {
+          L.marker(coord, { icon: pinCamion }).addTo(map);
+        }
       });
+      L.polyline(coordenadasRuta, {
+        color: "blue",
+        weight: 4,
+      }).addTo(map);
       break;
   }
 }
-
+function moverCamionYNotificar() {
+  camionMarker.setLatLng(nuevaCoordenadaCamion);
+  setTimeout(() => {
+    let notif = document.getElementById("notificacion-camion");
+    notif.classList.remove("notificacion-hidden");
+    notif.classList.add("notificacion-visible");
+  }, 5000);
+}
 // Llamadas a funciones
 botonesVolver.forEach((boton) => {
   boton.addEventListener("click", () => {
